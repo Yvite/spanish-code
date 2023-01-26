e8cb5e0c4f9c9e616af0d2df69602f738bda7cf2
Aarnav Bhat <aarnavpbhat@gmail.com>
2023-01-25 17:01:56 -0800

    Stored command seperatley

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-spanish_to_python = {
-    "suma": "sum",
-    "imprimir":"print",
-    "printar": "print",
-    "principal": "main",
-    "definir": "def",
-    "por": "for",
-    "cuando": "while",
-    "para": "for",
-    "en": "in",
-    "rango": "range",
-    "si": "if",
-    "sino": "else"
-}
+import json
+with open("spanish_to_python.json", "r") as f:
+    spanish_to_python = json.load(f)
 def translate_input(input_text):
     for spanish, python in spanish_to_python.items():
         input_text = input_text.replace(spanish, python)
     return input_text
 user_input = input("Enter a Python function in Spanish: ")
 translated_input = translate_input(user_input)
-exec(translated_input)
+eval(translated_input)
 
 
