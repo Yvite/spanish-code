06c5c2b7f784d0ef93e5c47370f34f6ecde486fb
Aarnav Bhat <aarnavpbhat@gmail.com>
2023-01-27 15:17:44 -0800

    Made it infinite lines

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import json
+
 with open("spanish_to_python.json", "r") as f:
     spanish_to_python = json.load(f)
+    
 def translate_input(input_text):
     for spanish, python in spanish_to_python.items():
         input_text = input_text.replace(spanish, python)
     return input_text
-user_input = input("Enter a Python function in Spanish: ")
-translated_input = translate_input(user_input)
-exec(translated_input)
 
-
+while True:
+    user_input = input("Enter a Python function in Spanish (or type 'end command' to exit): ")
+    if user_input == "esp end command":
+        break
+    translated_input = translate_input(user_input)
+    exec(translated_input)
+    if user_input != "end command":
+        user_input = input("Enter a Python function in Spanish (or type 'end command' to exit): ")
+        if user_input == "end command":
+            break
+        translated_input = translate_input(user_input)
+        exec(translated_input)
+        
